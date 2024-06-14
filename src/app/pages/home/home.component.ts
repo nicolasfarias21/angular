05d1a0b6f7bf6842e8832d842ff0dfcfd85be385
nicolasfarias21 <fariasnicolasofc@gmail.com>
2024-06-13 23:22:12 -0300

    feature: home componente

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from '../../shared/banner/banner.component';
+import { ContainerComponent } from '../../shared/container/container.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BannerComponent],
+  imports: 
+  [
+    BannerComponent,
+    ContainerComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
