cf1fa62c1cca01c9551d5f374719c44e293344ff
nicolasfarias21 <fariasnicolasofc@gmail.com>
2024-06-15 00:17:24 -0300

    feature: form-busca

---
+++ src/app/shared/form-busca/form-busca.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-busca',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-busca.component.html',
+  styleUrl: './form-busca.component.scss'
+})
+export class FormBuscaComponent {
+
+}
